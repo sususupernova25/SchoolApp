066acb1151f3e82c38b6dea399fe68b0d8c165cb
Moldir <mi1rorball@gmail.com>
2026-05-13 13:23:51 +0500

    Initial Commit

---
+++ SchoolApp/SchoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolAppApp.swift
+//  SchoolApp
+//
+//  Created by Moldir on 13.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct SchoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
