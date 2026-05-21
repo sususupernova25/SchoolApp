17556d88c0761b5a8ab0682c82bce093c3dc49a8
Moldir <mi1rorball@gmail.com>
2026-05-21 23:34:45 +0500

    students list + detail navigation

---
+++ SchoolApp/StudentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentDetailView.swift
+//  SchoolApp
+//
+//  Created by Moldir on 14.05.2026.
+//
+
+import SwiftUI
+
+struct StudentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentDetailView()
+}
---
+++ SchoolApp/StudentsView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentsView.swift
+//  SchoolApp
+//
+//  Created by Moldir on 14.05.2026.
+//
+
+import SwiftUI
+
+struct StudentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentsView()
+}
